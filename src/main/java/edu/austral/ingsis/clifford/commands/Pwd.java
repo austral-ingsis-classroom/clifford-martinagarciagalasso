b732100298377e3f58140063d0d2776498b9f23c
martinagarciagalasso <mgarciagalasso@mail.austral.edu.ar>
2024-06-23 19:02:45 -0300

    Pwd done

--- src/main/java/edu/austral/ingsis/clifford/commands/Pwd.java
+++ src/main/java/edu/austral/ingsis/clifford/commands/Pwd.java
@@ -1,8 +1,36 @@
 package edu.austral.ingsis.clifford.commands;
 
+import edu.austral.ingsis.clifford.FileSystem;
+import edu.austral.ingsis.clifford.directories.Directory;
+
+import java.util.Objects;
+
 public class Pwd implements Commands{
+    private final FileSystem fileSystem;
+
+    public Pwd(FileSystem fileSystem) {
+        this.fileSystem = fileSystem;
+    }
+
     @Override
     public String execute() {
-        return null;
+       return getroute(fileSystem);
     }
+
+
+    private String getroute(FileSystem fileSystem) {
+        Directory currentDir = fileSystem.getCurrentDirectory();
+        StringBuilder routeBuilder = new StringBuilder();
+
+        // Traverse parent directories and build the route
+        while (currentDir != null) {
+            routeBuilder.insert(0, "/" + currentDir.getName());
+            currentDir = currentDir.getParent();
+        }
+
+        return routeBuilder.toString();
+
+    }
+
+
 }
